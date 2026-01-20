153c488f66c83c73cb6cc7865a7801b53cc4ee34
swaparup36 <swaparup36@gmail.com>
2026-01-20 21:55:14 +0530

    cron job fixed and workflows fixed

--- scheduled-worker/src/jobs/monthlyTokenReset.ts
+++ scheduled-worker/src/jobs/monthlyTokenReset.ts
@@ -3,12 +3,28 @@ import prisma from "../db/prisma.js";
 export async function monthlyTokenReset() {
   console.log("[CRON] Monthly token reset started");
 
-  await prisma.user.updateMany({
-    data: {
-      tokenBalance: 1,
-      lastTokenResetAt: new Date()
+  const allUserTokenBalances = await prisma.userTokenBalance.findMany({});
+
+  for (const userTokenBalance of allUserTokenBalances) {
+    const userId = userTokenBalance.userId;
+    const subscription = await prisma.userSubscription.findFirst({
+      where: {
+        userId: userId,
+        status: "active",
+      },
+    });
+
+    if (!subscription) {
+      await prisma.userTokenBalance.updateMany({
+        where: {
+          userId: userId,
+        },
+        data: {
+          balance: 1,
+        }
+      });
     }
-  });
+  }
 
   console.log("[CRON] Monthly token reset completed");
 }
