754f98d4f03d330a3a383dd93289ee975ef9dc69
swaparup36 <swaparup36@gmail.com>
2026-01-22 01:40:44 +0530

    fix: conversion page invalid client use

--- app/app/[conversionId]/page.tsx
+++ app/app/[conversionId]/page.tsx
@@ -1,13 +1,12 @@
-"use client";
-
 import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import Converter from "@/components/converter"
 
 export default async function ConversionPage() {
   const { userId } = await auth();
 
   if (!userId) {
-    window.location.href = "/";
+    redirect("/");
   }
 
   return (
